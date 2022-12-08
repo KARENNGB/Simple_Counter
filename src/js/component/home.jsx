f5ef1d3e3c11d7521cef38737427fb09bab0b6d6
KARENNGB <karengutierrezb@hotmail.com>
2022-12-08 02:50:10 +0000

    Mi primer counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+function SecondsCounter(props) {
+  return (
+    <div className="BigCounter">
+      <div className="Clock">
+        <i className="fa-regular fa-clock"></i>
+      </div>
+      <div className="six">{props.digitSix % 10}</div>
+      <div className="five">{props.digitFive % 10}</div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
+    </div>
+  );
+}
+
+SecondsCounter.propTypes = {
+  digitSix: PropTypes.number,
+  digitFive: PropTypes.number,
+  digitFour: PropTypes.number,
+  digitThree: PropTypes.number,
+  digitTwo: PropTypes.number,
+  digitOne: PropTypes.number,
 };
 
-export default Home;
+let counter = 0;
+setInterval(function () {
+  const six = Math.floor(counter / 100000);
+  const five = Math.floor(counter / 10000);
+  const four = Math.floor(counter / 1000);
+  const three = Math.floor(counter / 100);
+  const two = Math.floor(counter / 10);
+  const one = Math.floor(counter / 1);
+  console.log(six, five, four, three, two, one);
+  counter++;
+  ReactDOM.render(
+    <SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six} />,
+    document.querySelector("#app")
+  );
+}, 1000);
+
+export default SecondsCounter;
